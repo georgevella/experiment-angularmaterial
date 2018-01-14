7975b4845a761cfa091407b47947ec673e1a3bb9
George Vella <george.vella@betssongroup.com>
2018-01-14 12:49:12 +0100

    Added vendor chunking

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -6,7 +6,6 @@ var helpers = require("./helpers");
 module.exports = {
   entry: {
     polyfills: "./src/polyfills.ts",
-    //    'vendor': './src/vendor.ts',
     main: "./src/main.ts",
     styles: ["./src/styles.css"]
   },
@@ -22,8 +21,10 @@ module.exports = {
         loaders: [
           {
             loader: "@ngtools/webpack",
-            options: { tsConfigPath: helpers.root("src", "tsconfig.app.json") }
-          } 
+            options: {
+              tsConfigPath: helpers.root("src", "tsconfig.app.json")
+            }
+          }
         ]
       },
       {
@@ -36,26 +37,17 @@ module.exports = {
         loader: "file-loader?name=assets/[name].[hash].[ext]"
       },
       {
-        test: /\.css$/,                            
+        test: /\.css$/,
         use: [
           //"exports-loader?module.exports.toString()",
           "to-string-loader",
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              import: false
-            }
-          }
+          "css-loader"
         ],
         exclude: [helpers.root("src/styles.css")]
       },
       {
         test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ],
+        use: ["style-loader", "css-loader"],
         include: [helpers.root("src/styles.css")]
       }
     ]
@@ -72,12 +64,46 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "src/index.html",
       xhtml: true,
-      minify: false
+      minify: false,
+      chunksSortMode: (left, right) => {
+        let entryPoints = [
+          // required stuff goes here
+          "inline",
+          "polyfills",
+          "sw-register",
+          // global styles
+          "styles",
+          // vendor and main always go last
+          "vendor",
+          "main"
+        ];
+
+        let leftIndex = entryPoints.indexOf(left.names[0]);
+        let rightindex = entryPoints.indexOf(right.names[0]);
+
+        if (leftIndex > rightindex) {
+          return 1;
+        } else if (leftIndex < rightindex) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }
     }),
+
     new webpack.optimize.CommonsChunkPlugin({
       minChunks: Infinity,
       name: "inline"
     }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      chunks: ["main"],
+      minChunks: (module, count) => {
+        return module.resource && /node_modules/.test(module.resource);
+      }
+    }),
+
     new webpack.optimize.CommonsChunkPlugin({
       name: "main",
       async: "common",
